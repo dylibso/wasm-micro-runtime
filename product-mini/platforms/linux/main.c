9453ed7e2da684ce2dc497d74244d9313ec14bd5
Gavin Hayes <gavin@dylibso.com>
2023-09-12 11:27:46 -0400

    fix: wamr building with cosmo a359de7893c33bfbd99aa82d570f97c55bf2b935

--- product-mini/platforms/linux/main.c
+++ product-mini/platforms/linux/main.c
@@ -2,7 +2,4 @@
  * Copyright (C) 2019 Intel Corporation.  All rights reserved.
  * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
  */
-// STATIC_YOINK("blink_linux_aarch64"); // for raspberry pi
-// STATIC_YOINK("blink_xnu_aarch64");
-STATIC_YOINK("zipos");
 #include "../posix/main.c"
